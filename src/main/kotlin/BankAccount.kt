6e32d480a2f22c3227fddfe9d2aef3b13c373175
Nabifr <Husninabilfareno07@gmail.com>
2023-03-06 21:43:51 +0700

    Reno

---
+++ src/main/kotlin/BankAccount.kt
@@ -0,0 +1,9 @@
+class BankAccount {
+    val Ballance:Int=50000
+    fun deposid():String{
+        return "ss"
+    }
+    fun withdraw():String{
+        return "ss"
+    }
+}
---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,7 @@
+class Person1 {
+    var name:String="Wahyudi"
+    var age:String="20"
+    fun introduce():String{
+        return "Hi, my name is $name dan umur $age years old!"
+    }
+}
